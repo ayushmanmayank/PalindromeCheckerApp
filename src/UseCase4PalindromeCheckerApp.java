b5155ebc5b4bc7ee27e33e93abd4f8463534427c
ayushmanmayank <abhinavprakash355@gmail.com>
2026-03-16 14:05:01 +0530

    Added UC4 implementation

---
+++ src/UseCase4PalindromeCheckerApp.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class UseCase4PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        // Convert string to character array
+        char[] arr = input.toCharArray();
+
+        int start = 0;
+        int end = arr.length - 1;
+
+        boolean isPalindrome = true;
+
+        // Two pointer comparison
+        while (start < end) {
+            if (arr[start] != arr[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+
+        if (isPalindrome) {
+            System.out.println("The given string is a Palindrome.");
+        } else {
+            System.out.println("The given string is NOT a Palindrome.");
+        }
+
+        sc.close();
+    }
+}
